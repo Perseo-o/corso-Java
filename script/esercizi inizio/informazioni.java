dbe41c9615f895b32f7f434ce69a06d903ca73fb
Perseo-o <marcello.pittau@gmail.com>
2023-02-28 11:42:05 +0100

    pulizia

---
+++ script/esercizi inizio/informazioni.java
@@ -0,0 +1,20 @@
+public class informazioni {
+    public static void main(String[] args){
+       /*  //otterrremo delle frasi
+        System.out.print("Marcello");
+        System.out.println("Pittau");
+        System.out.println("");
+        System.out.print("età:19");
+        System.out.println("    scorpione");
+        //ottenieamo un numero
+        System.out.println(3*3);
+    
+    int a=5;
+    String s="positivo";
+
+    System.out.println(a);
+    System.out.println(s);*/
+    String a="por", b="co", c;
+    c=a+b;
+    System.out.println(c);
+}}
---
+++ script/esercizi inizio/operazioniMath.java
@@ -0,0 +1,38 @@
+//import java.util.Scanner;
+
+public class operazioniMath {
+    public static void main(String[] args) {
+        /* 
+        Scanner myNum1 = new Scanner(System.in);
+        double num1;
+
+        System.out.println("dammi il primo numero: ");
+        num1= myNum1.nextDouble();
+
+        Scanner myNum2 = new Scanner(System.in);
+        double num2;
+
+        System.out.println("dammi il secondo numero: ");
+        num2= myNum2.nextDouble();
+
+        double randomNum=(int)(Math.random()*101);
+
+        System.out.println("il numero maggiore è: "+ Math.max(num1, num2));
+        System.out.println(randomNum+" è un numero randomico");
+        
+        int x=10;
+        int y=9;
+        System.out.println(x>y);
+        System.out.println(10>9);
+        String f="ciao";
+        System.out.println(f=="ciao");
+        */
+        String s1 = new String("Compare two strings in Java");
+        String s2 = new String("Compare two strings in Java");
+
+        // Restituisce true
+        System.out.println(s1 != null && s1.compareTo(s2) == 0);
+   // myNum1.close();
+   // myNum2.close();
+    }
+}
