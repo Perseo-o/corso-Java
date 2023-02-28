dc2e3897e866d07dd1dfeb419b88244786862994
Perseo-o <marcello.pittau@gmail.com>
2023-02-28 14:37:31 +0100

    qualche modifica

---
+++ script/Esempi/esempiClassi/secondo.java
@@ -0,0 +1,19 @@
+package script.Esempi.esempiClassi;
+
+public class secondo {
+    String x ="ciao";
+    int y=55;
+    public static void main(String[] args) {
+        /*secondo myObj1=new secondo();//Object 1
+        secondo myObj2=new secondo();//Object 2
+        myObj2.y=25;
+        System.out.println(myObj1.y);//Output 55
+        System.out.println(myObj2.y);//Output 25*/
+        /*secondo myObj1=new secondo();//Object 1
+        secondo myObj2=new secondo();//Object 2
+        myObj2.x="gargamella";
+        System.out.println(myObj1.x);//Output 55
+        System.out.println(myObj2.x);//Output 25*/
+        
+    }
+}
